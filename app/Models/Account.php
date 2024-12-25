9f6a210fe97a0cac16db6b08afc310fddcd5aa91
Bahman026 <Bahman026@gmail.com>
2024-12-25 23:57:57 +0330

    update account model
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\AccountNameEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
@@ -25,11 +27,20 @@ class Account extends Model
         'balance',
     ];
 
-    /**
-     * Get the user that owns the account.
-     */
+    protected function casts(): array
+    {
+        return [
+            'account_name' => AccountNameEnum::class,
+        ];
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
     }
 }
