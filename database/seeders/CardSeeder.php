f88e330d1e58b2f0456a4e45bc213b27d02a5355
Bahman026 <Bahman026@gmail.com>
2024-12-25 23:57:28 +0330

    implement card factory and seeder
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

---
+++ database/seeders/CardSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Card;
+use Illuminate\Database\Seeder;
+
+class CardSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Card::factory()->count(30)->create();
+    }
+}
