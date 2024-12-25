edd5002de40d9f19ac0c60da48d784582768ff71
Bahman026 <Bahman026@gmail.com>
2024-12-25 23:58:27 +0330

    change phone_number length to 20
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone_number', 16)->unique();
+            $table->string('phone_number', 20)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
