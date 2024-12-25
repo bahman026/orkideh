c6cb4819f386305022e3e7e76ae5f2feb6c4a675
Bahman026 <Bahman026@gmail.com>
2024-12-25 23:55:11 +0330

    add account_name in accounts table
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

--- database/migrations/2024_12_25_160102_create_accounts_table.php
+++ database/migrations/2024_12_25_160102_create_accounts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class);
             $table->string('account_number')->unique();
+            $table->string('account_name');
             $table->unsignedBigInteger('balance')->default(0);
             $table->timestamps();
         });
