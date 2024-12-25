d48fb38f9ebbe8fd3ff216f9aff0ab4fdef7f794
Bahman026 <Bahman026@gmail.com>
2024-12-25 20:03:27 +0330

    implement AccountFactory
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'account_number' => fake()->unique()->numerify('####################'),
+            'balance' => fake()->numberBetween(1000, 50000000),
+        ];
+    }
+}
