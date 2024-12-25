9cf6cb2377538ba15795be0cf1b424cfbdffb1c1
Bahman026 <Bahman026@gmail.com>
2024-12-26 00:19:11 +0330

    create transaction model
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

---
+++ app/Models/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'source_card_id',
+        'destination_card_id',
+        'amount',
+    ];
+
+    public function sourceCard(): BelongsTo
+    {
+        return $this->belongsTo(Card::class, 'source_card_id');
+    }
+
+    public function destinationCard(): BelongsTo
+    {
+        return $this->belongsTo(Card::class, 'destination_card_id');
+    }
+}
