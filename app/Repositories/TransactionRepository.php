cc871466a71527c7cc416f0e481f66e66718643c
Bahman026 <Bahman026@gmail.com>
2024-12-26 18:04:56 +0330

    move queries to repositories
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -6,6 +6,7 @@ namespace App\Repositories;
 
 use App\Models\Transaction;
 use App\Repositories\Interfaces\TransactionRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 
 class TransactionRepository implements TransactionRepositoryInterface
@@ -38,5 +39,24 @@ class TransactionRepository implements TransactionRepositoryInterface
     public function find($id)
     {
         return Transaction::query()->findOrFail($id);
+    }
+
+    public static function getUserTransactions(int $userId): Builder
+    {
+        return Transaction::query()
+            ->select(
+                'transactions.id',
+                'transactions.amount',
+                'transactions.source_card_id',
+                'transactions.destination_card_id',
+                'transactions.created_at'
+            )
+            ->join('cards', function ($join) {
+                $join->on('transactions.source_card_id', '=', 'cards.id')
+                    ->orOn('transactions.destination_card_id', '=', 'cards.id');
+            })
+            ->join('accounts', 'accounts.id', '=', 'cards.account_id')
+            ->where('accounts.user_id', $userId)
+            ->orderBy('transactions.created_at', 'DESC');
     }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Repositories;
 
 use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 
-class UserRepository
+class UserRepository implements UserRepositoryInterface
 {
     public function create($data)
     {
@@ -37,5 +39,19 @@ class UserRepository
     public function find($id)
     {
         return User::query()->findOrFail($id);
+    }
+
+    public function getTopUsers(): Builder
+    {
+        return User::query()
+            ->select('users.id', 'users.name')
+            ->join('accounts', 'accounts.user_id', '=', 'users.id')
+            ->join('cards', 'cards.account_id', '=', 'accounts.id')
+            ->join('transactions', function ($join) {
+                $join->on('transactions.source_card_id', '=', 'cards.id')
+                    ->orOn('transactions.destination_card_id', '=', 'cards.id');
+            })
+            ->groupBy('users.id')
+            ->orderByRaw('COUNT(transactions.id) DESC');
     }
 }
