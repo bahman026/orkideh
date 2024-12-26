14a5f7976f072fc343741f6f7c5e4ed60935011d
Bahman026 <Bahman026@gmail.com>
2024-12-26 21:25:59 +0330

    create lang files
    Signed-off-by: Bahman026 <Bahman026@gmail.com>

---
+++ lang/fa/email.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'your_account_has_been_debited' => 'حساب شما بدهکار شد.',
+    'your_account_has_been_credited' => 'حساب شما بستانکار شد.',
+    'transaction_id' => 'شناسه تراکنش',
+    'amount' => 'مبلغ',
+    'card_number' => 'شماره کارت',
+    'transaction_date' => 'تاریخ تراکنش',
+    'transaction_notification' => 'اطلاع‌رسانی تراکنش',
+];
